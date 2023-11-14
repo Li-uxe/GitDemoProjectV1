47239b4bd19cf66c8bf5cfb96dd2e12da25a3fab
Jincheng Li <jincheng.li1@ucalgary.ca>
2023-11-14 16:51:27 -0700

    First Commit

---
+++ src/Test1.java
@@ -0,0 +1,12 @@
+
+public class Test1 {
+	public static void main(String[] args) {
+		String hungary  = "yes";
+		if(hungary == "yes") {
+			System.out.println("Eat something");
+		}
+		else {
+			System.out.println("Do some work");
+		}
+	}
+}
